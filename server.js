6199271ac100bbe7e78784f6f2ad8795aa9e8fcb
Disha <bidisha2125@gmail.com>
2023-06-19 21:11:01 +0530

    Completed index.ejs

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const articleRouter = require('./routes/articles');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/articles', articleRouter);
 
 app.get('/',(req, res) =>{
-    res.send("Hello world")
+    const articles = [
+      {
+        title: "Test article",
+        createdAt: new Date(),
+        description: "Test description",
+      },
+      {
+        title: "Test article",
+        createdAt: new Date(),
+        description: "Test description2",
+      },
+    ];
+    res.render('index',{articles: articles} )
 });
 
-app.listen(5000)
+app.listen(5000);
