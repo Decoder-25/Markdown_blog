24876a879a021eb147ef121530786f5b6d798680
Disha <bidisha2125@gmail.com>
2023-06-20 17:18:20 +0530

    Set up the new.ejs file and solved some error

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const articleRouter = require('./routes/articles');
+const express = require("express");
+const articleRouter = require("./routes/articles");
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use('/articles', articleRouter);
+app.set("view engine", "ejs");
 
-app.get('/',(req, res) =>{
-    const articles = [
-      {
-        title: "Test article",
-        createdAt: new Date(),
-        description: "Test description",
-      },
-      {
-        title: "Test article",
-        createdAt: new Date(),
-        description: "Test description2",
-      },
-    ];
-    res.render('index',{articles: articles} )
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test article",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+    {
+      title: "Test article",
+      createdAt: new Date(),
+      description: "Test description2",
+    },
+  ];
+  res.render("articles/index", { articles: articles });
+  // res.send("h2")
 });
-
+app.use("/articles", articleRouter);
 app.listen(5000);
